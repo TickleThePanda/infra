88c11cdbc090bb761eac1dbbfc41779244e837e3
Panda Rushwood | TickleThePanda <panda@ticklethepanda.dev>
2023-11-26 20:22:43 +0000

    Add location history sa

---
+++ auth/g-tf-sa--location-history.tf
@@ -0,0 +1,27 @@
+
+resource "google_service_account" "tf_location_history_manager" {
+  account_id = "tf-location-history-manager"
+  display_name = "Location history manager"
+}
+
+resource "google_service_account_iam_binding" "tf_location_history_manager" {
+  service_account_id = google_service_account.tf_location_history_manager.name
+  role = "roles/iam.workloadIdentityUser"
+  members = [
+    "principal://iam.googleapis.com/${google_iam_workload_identity_pool.identity_pool.name}/subject/${local.location_history_tf_workspace}",
+  ]
+}
+
+
+resource "google_project_iam_member" "tf_location_history_manager" {
+  project = data.google_project.project.project_id
+  for_each = toset([
+    "roles/run.developer",
+    "roles/compute.loadBalancerAdmin",
+    "roles/storage.admin",
+    "roles/compute.publicIpAdmin",
+    "roles/certificatemanager.editor"
+  ])
+  role    = each.key
+  member  = google_service_account.tf_location_history_manager.member
+}
--- auth/tf-workspaces.tf
+++ auth/tf-workspaces.tf
@@ -1,4 +1,5 @@
 locals {
   tf_organisation_id = "org-y5WGsRE1mkDRyycK"
   auth_tf_workspace = "ws-QjzXiNt3Fd51RyR1"
+  location_history_tf_workspace = "ws-aUvVyP6sFnTrmN6p"
 }
